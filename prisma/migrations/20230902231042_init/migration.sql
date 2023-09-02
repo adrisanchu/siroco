5dc240132e394d824fb2f9451d5ab3d20b1c8cdb
adrisanchu <adrian.sanchez@ree.es>
2023-09-03 01:18:36 +0200

    #Prisma #Setup: add SQLite DB + A first schema

---
+++ prisma/migrations/20230902231042_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "alias" TEXT NOT NULL,
+    "startDate" TEXT NOT NULL,
+    "description" TEXT
+);
