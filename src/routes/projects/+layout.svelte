c1ed74ed39a67a7c0486b63d8d1f3ecec0b55873
adrisanchu <adrian.sanchez@ree.es>
2023-09-02 19:38:49 +0200

    #Projects: initial setup

---
+++ src/routes/projects/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { AppShell, AppBar, Avatar } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Siroco</strong></svelte:fragment
+			>
+			<svelte:fragment slot="trail"
+				><Avatar initials="AS" width="w-10" background="bg-primary-500" /></svelte:fragment
+			>
+		</AppBar>
+	</svelte:fragment>
+
+	<!-- Router Slot -->
+	<div class="container p-6 mx-auto">
+		<slot />
+	</div>
+</AppShell>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+	<title>Siroco - Proyectos</title>
+</svelte:head>
+
+<div class="container h-full mx-auto gap-8 flex flex-col">
+	<div class="flex items-center justify-between">
+		<h2>Proyectos</h2>
+		<a href={`${$page.url.pathname}/new`} class="btn variant-ghost-primary">Nuevo Proyecto</a>
+	</div>
+	<p>Aún no hay proyectos...</p>
+</div>
