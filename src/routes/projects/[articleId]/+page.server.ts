793d14b7605054d277d454f4335e54c35296cf7c
adrisanchu <adrian.sanchez@ree.es>
2023-09-04 18:17:00 +0200

    #Projects #Edit: Finishing CRUD functionality for projects

---
+++ src/routes/projects/[articleId]/+page.server.ts
@@ -0,0 +1,55 @@
+import { prisma } from '$lib/server/prisma';
+import { fail, error, redirect } from '@sveltejs/kit';
+import type { Actions } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ params }) => {
+	const getProject = async () => {
+		const project = await prisma.project.findUnique({
+			where: {
+				id: Number(params.articleId)
+			}
+		});
+		if (!project) {
+			throw error(404, 'Project not found');
+		}
+		return project;
+	};
+	return {
+		project: getProject()
+	};
+};
+
+export const actions: Actions = {
+	updateProject: async ({ request, params }) => {
+		console.log('===updateProject:');
+		const data = await request.formData();
+		const name: string | undefined = data.get('name')?.toString();
+		const code: number | undefined = Number(data.get('code')?.toString());
+		const alias: string | undefined | null = data.get('alias')?.toString() || null;
+		const startDate: string | undefined = data.get('startDate')?.toString();
+		const description: string | undefined | null = data.get('description')?.toString() || null;
+
+		try {
+			await prisma.project.update({
+				where: {
+					id: Number(params.articleId)
+				},
+				data: {
+					name,
+					code,
+					alias,
+					startDate,
+					description
+				}
+			});
+		} catch (err) {
+			console.error(err);
+			return fail(500, { message: 'Something went wrong' });
+		}
+		// success!
+		console.log('===updateProject:ok');
+		throw redirect(303, '/projects');
+		// return { status: 200 };
+	}
+};
---
+++ src/routes/projects/[articleId]/+page.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	export let data: PageData;
+
+	$: ({ project } = data);
+</script>
+
+<svelte:head>
+	<title>Siroco - Editar Proyecto</title>
+</svelte:head>
+
+<div class="container h-full mx-auto gap-8 flex flex-col">
+	<form method="POST" action="?/updateProject" class="card p-4 flex flex-col gap-3">
+		<h2>Editando Proyecto: {project.name}</h2>
+
+		<label class="label">
+			<span>Título</span>
+			<input class="input" name="name" type="text" value={project.name} />
+		</label>
+
+		<label class="label">
+			<span>Alias</span>
+			<input class="input" name="alias" type="text" value={project.alias ?? ''} />
+		</label>
+
+		<label class="label">
+			<span>Código</span>
+			<input class="input" name="code" type="number" value={project.code} />
+		</label>
+
+		<label class="label">
+			<span>Fecha de origen</span>
+			<input class="input" name="startDate" title="Input (date)" type="date" value={project.startDate} />
+		</label>
+
+		<label class="label">
+			<span>Descripción</span>
+			<textarea
+				value={project.description}
+				name="description"
+				class="textarea"
+				rows="4"
+				placeholder="Introduce una descripción..."
+			/>
+		</label>
+		<button type="submit" class="btn variant-ghost-primary self-end">Guardar Proyecto</button>
+	</form>
+</div>
