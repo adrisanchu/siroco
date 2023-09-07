37a9165991ac8f5b92d3832e97961252d28f7929
adrisanchu <adrian.sanchez@ree.es>
2023-09-07 22:37:35 +0200

    #Project #Notes - change layout + add button to project notes

--- src/routes/projects/[projectId]/+page.svelte
+++ src/routes/projects/[projectId]/+page.svelte
@@ -19,45 +19,67 @@
 		</ol>
 		<a href={`${$page.url.pathname}/edit`} class="btn variant-ghost-secondary">Editar Proyecto</a>
 	</div>
-	<div class="card p-4 grid grid-cols-5 gap-2">
-		<div class="text-right">
-			<h4>Título</h4>
+	<h3 class="h3">Información General</h3>
+	<div class="card">
+		<div class="p-2 flex">
+			<div class="text-right mx-2 fixed-width">
+				<h4>Título</h4>
+			</div>
+
+			<div class="mx-4 flex-grow">
+				<span>{project.name}</span>
+			</div>
 		</div>
 
-		<div class="col-span-4">
-			<span>{project.name}</span>
+		<div class="p-2 flex">
+			<div class="text-right mx-2 fixed-width">
+				<h4>Alias</h4>
+			</div>
+
+			<div class="mx-4 flex-grow">
+				<span>{project.alias ?? '-'}</span>
+			</div>
 		</div>
 
-		<div class="text-right">
-			<h4>Alias</h4>
+		<div class="p-2 flex">
+			<div class="text-right mx-2 fixed-width">
+				<h4>Código</h4>
+			</div>
+
+			<div class="mx-4 flex-grow">
+				<span>{project.code}</span>
+			</div>
 		</div>
 
-		<div class="col-span-4">
-			<span>{project.alias ?? '-'}</span>
+		<div class="p-2 flex">
+			<div class="text-right mx-2 fixed-width">
+				<h4>Fecha de origen</h4>
+			</div>
+
+			<div class="mx-4 flex-grow">
+				<span>{project.startDate}</span>
+			</div>
 		</div>
 
-		<div class="text-right">
-			<h4>Código</h4>
-		</div>
+		<div class="p-2 flex">
+			<div class="text-right mx-2 fixed-width fijado">
+				<h4>Descripción</h4>
+			</div>
 
-		<div class="col-span-4">
-			<span>{project.code}</span>
-		</div>
-
-		<div class="text-right">
-			<h4>Fecha de origen</h4>
-		</div>
-
-		<div class="col-span-4">
-			<span>{project.startDate}</span>
-		</div>
-
-		<div class="text-right">
-			<h4>Descripción</h4>
-		</div>
-
-		<div class="col-span-4">
-			<span>{project.description}</span>
+			<div class="mx-4 flex-grow whitespace-pre-wrap">
+				<span>{project.description ?? '-'}</span>
+			</div>
 		</div>
 	</div>
+
+	<div class="flex justify-between">
+		<h3 class="h3">Notas</h3>
+		<a href={`${$page.url.pathname}/notes/new`} class="btn variant-ghost-primary">Nueva Nota</a>
+	</div>
 </div>
+
+<style>
+	.fixed-width {
+		min-width: 7rem;
+	}
+</style>
